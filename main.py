c0462a0783206b1b4c27e35894b359be57fb641e
Pradeep Bansiwal <46450532+pradeeps9@users.noreply.github.com>
2022-01-04 21:38:34 +0530

    shapes
    Drawing shapes from Triangle to Octagon consecutively.

--- main.py
+++ main.py
@@ -18,3 +18,30 @@ for x in range(15):
     tim.forward(10)
     tim.pd()
     
+    
+# Draw shapes from triangle to decagon
+no_of_sides = 3
+colours = ["royal blue", "teal", "firebrick", "dark orchid", "orange red", "green yellow", "crimson", "midnight blue", ]
+# Setting up turtle
+tim.penup()
+tim.left(135)
+tim.forward(100)
+tim.right(135)
+tim.pendown()
+
+# Going to position to draw shapes
+Setting (X,Y)
+tim.setx(-100)
+tim.sety(100)
+
+# Starting to draw shapes 
+for _ in range(3, 11):
+    angle = 360 / no_of_sides
+    tim.color(random.choice(colours))
+
+    for _ in range(no_of_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+    no_of_sides += 1
+
